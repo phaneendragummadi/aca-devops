26e331b0c5c8baadd4831d6f6cd479211d081baf
HelenaCoppieters <86767115+HelenaCoppieters@users.noreply.github.com>
2021-11-08 15:44:12 +0100

    Init repo with DevOps repo https://dev.azure.com/codit/Codit%20Integr… …ation%20Factory/_git/ARMForDummies

---
+++ src/ArmForDummies.App/ArmForDummies.App/Program.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+
+namespace ArmForDummies.App
+{
+    public class Program
+    {
+        public static IConfiguration Configuration { get; set; }
+
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args)
+        {
+            var builder =  WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+
+            return builder;
+        }
+    }
+}
