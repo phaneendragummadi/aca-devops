e59979d9661a2202185ee4b76edaa5f189f2405c
gitName <a@b.c>
2022-02-03 10:12:09 +0100

    change appsettings for security

--- src/Test/DevOps.App.Tests/EntryptorUnitTests.cs
+++ src/Test/DevOps.App.Tests/EntryptorUnitTests.cs
@@ -13,14 +13,14 @@ namespace DevOps.App.Tests
         [Fact]
         public void Encrypt_Value_Text_Returns()
         {
-            KeyVaultSecret keyVaultSecret = SecretModelFactory.KeyVaultSecret(new SecretProperties("EncryptionKey"), "EC545C08-43AF-4B49-965F-75B27300CFA0");
+            KeyVaultSecret keyVaultSecret = SecretModelFactory.KeyVaultSecret(new SecretProperties("<encryptionKey>"), "<encryptionKeyValue>");
             Azure.Response<KeyVaultSecret> response = Azure.Response.FromValue(keyVaultSecret, Mock.Of<Azure.Response>());
 
             var encryptor = new Encryptor(response.Value.ToString());
            
             var encryptedText = encryptor.Encrypt("Text");
             
-            Assert.Equal("R5IvW8qLrOo4ChFmjRzc+Q==", encryptedText);
+            Assert.Equal("<encryptionKeyValue>", encryptedText);
         }
     }
 }
