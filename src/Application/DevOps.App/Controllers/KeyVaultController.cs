06bb9f1ce6c8feb9ddf9c715ae02aca30a3b499a
gitName <a@b.c>
2022-05-18 10:44:29 +0200

    -converted web api to .NET 6.0

---
+++ src/Application/DevOps.App/Controllers/KeyVaultController.cs
@@ -0,0 +1,47 @@
+﻿using DevOps.App.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+//example from https://www.loginradius.com/blog/async/guest-post/using-azure-key-vault-with-an-azure-web-app-in-c-sharp/
+
+namespace DevOps.App.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class KeyVaultController : ControllerBase
+    {
+        private readonly IKeyVaultManager _secretManager;
+        public KeyVaultController(IKeyVaultManager secretManager)
+        {
+            _secretManager = secretManager;
+        }
+        [HttpGet]
+        public async Task<IActionResult> Get([FromQuery] string secretName)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(secretName))
+                {
+                    return BadRequest();
+                }
+                string secretValue = await
+                _secretManager.GetSecret(secretName);
+                if (!string.IsNullOrEmpty(secretValue))
+                {
+                    return Ok(secretValue);
+                }
+                else
+                {
+                    return NotFound("Secret key not found.");
+                }
+            }
+            catch(Exception ex)
+            {
+                return BadRequest("Error: Unable to read secret - " + ex.Message);
+            }
+        }
+    }
+}
---
+++ src/Application/DevOps.App/Repositories/EncryptedTableStorageRepository.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Threading.Tasks;
+using Azure.Data.Tables;
+using Microsoft.Extensions.Configuration;
+
+namespace DevOps.App.Repositories
+{
+    public class EncryptedTableStorageRepository
+    {
+        private const string EncryptionEntityPartitionKey = "DevOps";
+        private const string EncryptionEntityRowKey = "Introduction";
+        private readonly IConfiguration _configuration;
+      
+        private readonly TableClient _tableClient;
+
+
+        public EncryptedTableStorageRepository(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            var connectionString = _configuration.GetConnectionString("StorageAccount.ConnectionString");
+            var tableName = _configuration["StorageAccount.TableName"];
+            _tableClient = new TableClient(connectionString, tableName);
+        }
+        private TableEntity ToTableEntity(string key, string value)
+        {
+            TableEntity tableEntity;
+            tableEntity = new TableEntity
+            {
+                PartitionKey = EncryptionEntityPartitionKey,
+                RowKey = EncryptionEntityRowKey
+            };
+            tableEntity.Add(key, value);
+            return tableEntity;
+        }
+        public async Task UpdateAsync(string entityKey, string entityValue)
+        {
+            await _tableClient.UpsertEntityAsync(ToTableEntity(entityKey, entityValue));
+        }
+
+        public async Task<TableEntity> GetAsync()
+        {
+            var result = await _tableClient.GetEntityAsync<TableEntity>(EncryptionEntityPartitionKey, EncryptionEntityRowKey);
+            await _tableClient.CreateIfNotExistsAsync();
+
+            if (result == null)
+            {
+                throw new InvalidOperationException("No entity was found. Please insert an entity first.");
+            }
+           
+            return result;
+        }
+    }
+}
