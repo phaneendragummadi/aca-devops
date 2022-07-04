27f5e35adf66c5c2c761c295b7d6b9d29af8125f
gitName <a@b.c>
2022-07-04 18:06:15 +0200

    -added 2 web app samples

---
+++ src/Application/DevOpsSample.WebApp/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using DevOpsSample.WebApp.Models;
+using Microsoft.Extensions.Configuration;
+
+namespace DevOpsSample.WebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IConfiguration _configuration;
+
+        public HomeController(ILogger<HomeController> logger,
+            IConfiguration configuration)
+        {
+            _logger = logger;
+            _configuration = configuration;
+        }
+
+        public IActionResult Index()
+        {
+            ViewData["myKeyValue"] = _configuration["MyKey"];
+            ViewData["positionTitle"] = _configuration["Position:Title"];
+            ViewData["positionName"] = _configuration["Position:Name"];
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
