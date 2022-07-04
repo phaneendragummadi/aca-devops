884e34c16e938cb82b810dae6a02a03019ddd0dd
gitName <a@b.c>
2022-07-04 15:47:44 +0200

    Arcus .Net 6 demo

---
+++ src/Application/Arcus.Demo.WebAPI/Controllers/ConfigValuesController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System.Collections.Generic;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Arcus.Demo.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ConfigValuesController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        public ConfigValuesController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        // GET: api/<ConfigValuesController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            string vaulturi = _configuration.GetValue<string>("VaultUri");
+            return new string[] { vaulturi };
+           // return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<ConfigValuesController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<ConfigValuesController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<ConfigValuesController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<ConfigValuesController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ src/Application/Arcus.Demo.WebAPI/Controllers/HealthController.cs
@@ -0,0 +1,61 @@
+﻿using System.Threading.Tasks;
+using Arcus.Demo.WebAPI.Models;
+using Arcus.WebApi.Logging;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using GuardNet;
+using Arcus.Demo.WebAPI.ExampleProviders;
+using Swashbuckle.AspNetCore.Filters;
+
+namespace Arcus.Demo.WebAPI.Controllers
+{
+    /// <summary>
+    /// API endpoint to check the health of the application.
+    /// </summary>
+    [ApiController]
+    [Route("api/v1/health")]
+    public class HealthController : ControllerBase
+    {
+        private readonly HealthCheckService _healthCheckService;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="HealthController"/> class.
+        /// </summary>
+        /// <param name="healthCheckService">The service to provide the health of the API application.</param>
+        public HealthController(HealthCheckService healthCheckService)
+        {
+            Guard.NotNull(healthCheckService, nameof(healthCheckService));
+
+            _healthCheckService = healthCheckService;
+        }
+
+        /// <summary>
+        ///     Get Health
+        /// </summary>
+        /// <remarks>Provides an indication about the health of the API.</remarks>
+        /// <response code="200">API is healthy</response>
+        /// <response code="503">API is unhealthy or in degraded state</response>
+        [HttpGet(Name = "Health_Get")]
+        [RequestTracking(500, 599)]
+        [ProducesResponseType(typeof(ApiHealthReport), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ApiHealthReport), StatusCodes.Status503ServiceUnavailable)]
+        [SwaggerResponseHeader(200, "RequestId", "string", "The header that has a request ID that uniquely identifies this operation call")]
+        [SwaggerResponseHeader(200, "X-Transaction-Id", "string", "The header that has the transaction ID is used to correlate multiple operation calls.")]
+        [SwaggerResponseExample(200, typeof(HealthReportResponseExampleProvider))]
+        public async Task<IActionResult> Get()
+        {
+            HealthReport healthReport = await _healthCheckService.CheckHealthAsync();
+            ApiHealthReport json = ApiHealthReport.FromHealthReport(healthReport);
+
+            if (healthReport.Status == HealthStatus.Healthy)
+            {
+                return Ok(json);
+            }
+            else
+            {
+                return StatusCode(StatusCodes.Status503ServiceUnavailable, json);
+            }
+        }
+    }
+}
