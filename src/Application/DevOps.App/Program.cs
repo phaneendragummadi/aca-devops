885d126289a7263e19ea953782e7e69d639e47df
gitName <a@b.c>
2022-07-06 16:28:59 +0200

    -upgraded app to .Net Core 3.1

--- src/Application/DevOps.App/Program.cs
+++ src/Application/DevOps.App/Program.cs
@@ -1,6 +1,11 @@
-﻿using Microsoft.AspNetCore;
-using Microsoft.AspNetCore.Hosting;
+﻿using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace DevOps.App
 {
@@ -10,15 +15,14 @@ namespace DevOps.App
 
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            CreateHostBuilder(args).Build().Run();
         }
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args)
-        {
-            var builder =  WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
-
-            return builder;
-        }
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
     }
 }
