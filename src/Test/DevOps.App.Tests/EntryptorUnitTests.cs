7319a53f06c45b323731e1fe7c0f6709ecbd5805
gitName <a@b.c>
2022-07-11 08:51:06 +0200

    -upgrades to newer versions

---
+++ src/Test/DevOps.App.Tests/EntryptorUnitTests.cs
@@ -0,0 +1,27 @@
+using Azure.Security.KeyVault.Secrets;
+using DevOps.App.Encryption;
+using Moq;
+using System;
+using System.Threading;
+using Xunit;
+
+namespace DevOps.App.Tests
+{
+    [Trait("Category", "Unit")]
+    public class EntryptorUnitTests
+    {
+        [Fact]
+        public void Encrypt_Value_Text_Returns() 
+        {
+            KeyVaultSecret keyVaultSecret = SecretModelFactory.KeyVaultSecret(new SecretProperties("<encryptionKey>"), "<encryptionKeyValue>");
+            Azure.Response<KeyVaultSecret> response = Azure.Response.FromValue(keyVaultSecret, Mock.Of<Azure.Response>());
+
+            var encryptor = new Encryptor(response.Value.ToString());
+           
+            var encryptedText = encryptor.Encrypt("<encryptionKeyValue>");
+            var decryptedText = encryptor.Decrypt(encryptedText);
+
+            Assert.Equal(decryptedText, "<encryptionKeyValue>");
+        }
+    }
+}
